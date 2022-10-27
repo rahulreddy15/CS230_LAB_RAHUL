a61b884677330dbf9d7358ae36d8175fa7ed6ff3
rahulreddy15 <arahulreddy15@gmail.com>
2022-10-27 11:11:11 -0400

    Firebase Deployment Switching Backend fetching to Firebase Library

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
 export const environment = {
   production: true
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7_gmEwXHGtmZOweWZijkVc1bLgx5DoUw",
+  authDomain: "insomniac-games-app.firebaseapp.com",
+  databaseURL: "https://insomniac-games-app-default-rtdb.firebaseio.com",
+  projectId: "insomniac-games-app",
+  storageBucket: "insomniac-games-app.appspot.com",
+  messagingSenderId: "314711490464",
+  appId: "1:314711490464:web:89f92a20cc01a7a4d76ec5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,22 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7_gmEwXHGtmZOweWZijkVc1bLgx5DoUw",
+  authDomain: "insomniac-games-app.firebaseapp.com",
+  databaseURL: "https://insomniac-games-app-default-rtdb.firebaseio.com",
+  projectId: "insomniac-games-app",
+  storageBucket: "insomniac-games-app.appspot.com",
+  messagingSenderId: "314711490464",
+  appId: "1:314711490464:web:89f92a20cc01a7a4d76ec5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
